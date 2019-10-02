6102831c3621cf8f9e805d48a99917f717d13860
smartankur4u <ankurnarkhede@gmail.com>
2019-10-02 08:05:30 +0530

    Added tests

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
+const assert = require('assert');
 const tab_to_space = require('./index')
+
+describe('Tabs to spaces', () => {
+  it('should return text indented by spaces', () => {
+    let text = '\t\t[FirstName]\t\t\t\t=> TEST\n' +
+      '        [LastName]              => TEST\n'
+    assert.strictEqual(tab_to_space(text, 4),
+      '        [FirstName]             => TEST\n' +
+      '        [LastName]              => TEST\n');
+  });
+});
