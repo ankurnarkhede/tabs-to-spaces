275d3e7aee8d978c6f3faef073ead724bbbd8170
sakshi-patil <sakshipatil2805@gmail.com>
2019-10-02 12:39:52 +0530

    Added travis ci

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-const assert = require('assert');
-const tab_to_space = require('./index')
+const assert = require('assert')
+const tabToSpace = require('./index')
 
 describe('Tabs to spaces', () => {
   it('should return text indented by spaces', () => {
-    let text = '\t\t[FirstName]\t\t\t\t=> TEST\n' +
+    const text = '\t\t[FirstName]\t\t\t\t=> TEST\n' +
       '        [LastName]              => TEST\n'
-    assert.strictEqual(tab_to_space(text, 4),
+    assert.strictEqual(tabToSpace(text, 4),
       '        [FirstName]             => TEST\n' +
-      '        [LastName]              => TEST\n');
-  });
-});
+      '        [LastName]              => TEST\n')
+  })
+})
